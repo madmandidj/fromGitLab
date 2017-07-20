625b875286b5317bd9ad3cc16119a06564cdae50
Eyal Alon <madmandidj@gmail.com>
2017-07-20 16:05:11 +0300

    Add Bubble Sort Files

---
+++ ds/BubbleSortVec1/BubbleSortVec.h
@@ -0,0 +1,20 @@
+#ifndef __BUBBLESORTVEC_H__
+#define __BUBBLESORTVEC_H__
+
+/*
+Filename: 		BubbleSort.h 
+Description:	BubbleSort API given by Ronny
+Created: 		20/07/17
+Created by:		Eyal Alon
+Last modified: 	20/07/17
+Modified by:	Eyal Alon
+*/
+#include "ADTDefs.h"
+
+typedef struct Vector Vector;
+
+ADTErr 	BubbleSort(Vector* _v);
+
+
+
+#endif /* #ifndef __BUBBLESORTVEC_H__ */
