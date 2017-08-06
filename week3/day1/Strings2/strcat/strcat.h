d9478a14768e26e79e6b13aedf2436f09dd133ba
Eyal_Alon <eyal_alon-2016@Eyals-MacBook-Pro-2.local>
2017-08-06 08:45:36 +0300

    week8 day 1 restructured

--- week3/day1/Strings2/strcat/strcat.h
+++
@@ -1,9 +0,0 @@
-#ifndef STRCAT_H
-#define STRCAT_H
-
-/*
-Appends the string pointed to by src to the end of string pointed by dest
-*/
-char *My_strcat(char *dest, const char *src);
-
-#endif
--- week3/day1/Strings2/strcmp/strcmp.h
+++
@@ -1,8 +0,0 @@
-#ifndef STRCMP_H
-#define STRCMP_H
-#define MY_STRING_CAP 1024
-
-int My_strcmp(const char *str1, const char *str2);
-
-
-#endif
