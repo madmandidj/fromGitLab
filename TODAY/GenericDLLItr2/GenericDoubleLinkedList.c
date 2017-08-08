56e7b99a4eae09ed687f07df0bb18b463e8d71a5
Eyal_Alon <eyal_alon-2016@Eyals-MacBook-Pro-2.local>
2017-08-08 08:06:43 +0300

    week9 day 3 morning experis

---
+++ TODAY/GenericDLLItr2/GenericDoubleLinkedList.c
@@ -0,0 +1,559 @@
+/*
+Filename: 		GenericDoubleLinkedList.c 
+Description:	Implementation of generic DLL API
+Created: 		06/08/17
+Created by:		Eyal Alon
+Last modified: 	07/08/17
+Modified by:	Eyal Alon
+*/
+
+#include <stdio.h>
+#include "list_itr.h"
+#define MAGIC_NUM 0x00002000 /* Generic DLL Magic Num is 0x00002000*/
+#define TRUE 1
+#define FALSE 0
+#define IS_A_LIST(L) ((L) && (L)->m_magicNum == MAGIC_NUM) 
+#define IS_EMPTY(L) (LIST_FIRST((L)) == LIST_END((L))))
+#define LIST_FIRST(L) ((L)->m_head.m_next)
+#define LIST_LAST(L) ((L)->m_tail.m_prev)
+#define LIST_END(L) (&(L)->m_tail)
+
+
+/*
+TODO: factorize all functions
+TODO: static functions that deal with node allocation shouldnt return errors. errors should be returned from api functions
+*/
+
+typedef struct Node Node;
+
+struct Node
+{
+	void* 	m_data;
+	Node*	m_next;
+	Node*	m_prev;
+};
+
+struct List
+{
+	size_t m_magicNum;
+	Node m_head;
+	Node m_tail;
+};
+
+
+
+
+
+
+
+
+
+/******
+LIST CREATE
+******/
+List* 	ListCreate(void)
+{
+	List* list;
+	
+	list = malloc(sizeof(List));
+	if (NULL == list)
+	{
+		return NULL;
+	}
+	list->m_head.m_next = &(list->m_tail);
+	list->m_head.m_prev = NULL;
+	list->m_tail.m_prev = &(list->m_head);
+	list->m_tail.m_next = NULL;
+	list->m_magicNum = MAGIC_NUM;
+	return list;
+}
+
+
+
+
+
+
+
+
+
+/******
+LIST DESTROY
+******/
+void ListDestroy(List** _list, UserActionFunc _destroyFunc)
+{
+	Node* curN;
+	
+	if (NULL != _list && NULL != *_list && (*_list)->m_magicNum)
+	{
+		if ((*_list)->m_head.m_next != &((*_list)->m_tail))
+		{
+			curN = (*_list)->m_head.m_next;
+			while(curN != &((*_list)->m_tail))
+			{
+				curN = curN->m_next;
+				if (_destroyFunc != NULL)
+				{
+					_destroyFunc(curN->m_prev->m_data, NULL);
+				}
+				free(curN->m_prev);
+			}
+		}
+		(*_list)->m_magicNum = 0;
+		free(*_list);
+		*_list = NULL;
+	}
+	return;
+}
+
+
+
+
+
+
+
+
+/******
+LIST PUSH HEAD
+******/
+ListErrors	ListPushHead(List* _list, void* _data)
+{
+	Node* node;
+	
+	if (IS_A_LIST(_list))
+	{
+		node = malloc(sizeof(Node));
+		if (NULL == node)
+		{
+			return LIST_ALLOCATION_FAILED;
+		}
+		node->m_data = _data;
+		node->m_next = _list->m_head.m_next;
+		node->m_prev = &(_list->m_head);
+		node->m_prev->m_next = node;
+		node->m_next->m_prev = node;
+	}
+	else
+	{
+		return LIST_UNINITIALIZED;	
+	}
+	return LIST_OK;
+}
+
+
+
+
+
+
+
+
+
+/******
+LIST PUSH TAIL
+******/
+ListErrors	ListPushTail(List* _list, void* _data)
+{
+	Node* node;
+	
+	if (IS_A_LIST(_list))
+	{
+		node = malloc(sizeof(Node));
+		if (NULL == node)
+		{
+			return LIST_ALLOCATION_FAILED;
+		}
+		node->m_data = _data;
+		node->m_prev = _list->m_tail.m_prev;
+		node->m_next = &(_list->m_tail);
+		node->m_next->m_prev = node;
+		node->m_prev->m_next = node;
+	}
+	else
+	{
+		return LIST_UNINITIALIZED;	
+	}
+	return LIST_OK;
+}
+
+
+
+
+
+
+
+
+
+/******
+LIST POP HEAD
+******/
+ListErrors ListPopHead(List* _list, void* *_data)
+{
+	Node* freeNode;
+	if (NULL == _data)
+	{
+		return LIST_INV_ARG;
+	}
+	if (IS_A_LIST(_list))
+	{
+		if (_list->m_head.m_next != &(_list->m_tail))
+		{
+			*_data = _list->m_head.m_next->m_data;
+			freeNode = _list->m_head.m_next;
+			_list->m_head.m_next->m_next->m_prev = &(_list->m_head);
+			_list->m_head.m_next = _list->m_head.m_next->m_next;
+			free(freeNode);
+		}
+		else
+		{
+			return LIST_IS_EMPTY;
+		}
+	}
+	else
+	{
+		return LIST_UNINITIALIZED;	
+	}
+	return LIST_OK;
+}
+
+
+
+
+
+
+
+
+
+/******
+LIST POP TAIL
+******/
+ListErrors ListPopTail(List* _list, void* *_data)
+{
+	Node* freeNode;
+	
+	if (NULL == _data)
+	{
+		return LIST_INV_ARG;
+	}
+	if (IS_A_LIST(_list))
+	{
+		if (_list->m_head.m_next != &(_list->m_tail))
+		{
+			*_data = _list->m_tail.m_prev->m_data;
+			freeNode = _list->m_tail.m_prev;
+			_list->m_tail.m_prev->m_prev->m_next = &(_list->m_tail);
+			_list->m_tail.m_prev =  _list->m_tail.m_prev->m_prev;
+			free(freeNode);
+		}
+		else
+		{
+			return LIST_IS_EMPTY;
+		}
+	}
+	else
+	{
+		return LIST_UNINITIALIZED;	
+	}
+	return LIST_OK;
+}
+
+
+
+
+
+
+
+
+
+/******
+LIST COUNT ITEMS
+******/
+size_t	ListCountItems(const List* _list)
+{
+	Node* curN;
+	
+	size_t nItems = 0;
+	if (IS_A_LIST(_list))
+	{
+		if (_list->m_head.m_next != &(_list->m_tail))
+		{
+			curN = _list->m_head.m_next;
+			while(curN != &(_list->m_tail))
+			{
+				++nItems;
+				curN = curN->m_next;
+			}
+		}
+	}
+	return nItems;
+}
+
+
+
+
+
+
+
+
+
+/******
+LIST IS EMPTY
+******/
+int		ListIsEmpty(const List* _list)
+{
+	int result = TRUE;
+	if (!IS_A_LIST(_list))
+	{
+		result = -1;
+	}
+	else if (_list->m_head.m_next != &(_list->m_tail))
+	{
+		result = FALSE;
+	}
+	return result;
+}
+
+
+
+
+
+
+
+
+
+/******
+LIST FIND FIRST FORWARD
+******/
+ListErrors FindFirstForward(const List* _list, PredicateFunc _predicateFunc, void* _context, void* *_item)
+{
+	Node* curN;
+	int isFound = FALSE;
+	if (!IS_A_LIST(_list))
+	{
+		return LIST_UNINITIALIZED;
+	}
+	if (NULL == _predicateFunc || NULL == _item )
+	{
+		return LIST_INV_ARG;
+	}
+	if (_list->m_head.m_next == &(_list->m_tail))
+	{
+		return LIST_IS_EMPTY;
+	}
+	curN = _list->m_head.m_next;
+	while(curN != &(_list->m_tail))
+	{
+		isFound = _predicateFunc(curN->m_data, _context);
+		if (isFound)
+		{
+			*_item = curN->m_data;
+			return LIST_OK;
+		}
+		curN = curN->m_next;
+	}
+	*_item = NULL;
+	return LIST_ITEM_NOT_FOUND;
+}
+
+
+
+
+
+
+
+
+
+/******
+LIST FIND FIRST BACKWARD
+******/
+ListErrors FindFirstBackward(const List* _list, PredicateFunc _predicateFunc, void* _context, void* *_item)
+{
+	Node* curN;
+	int isFound = FALSE;
+	if (!IS_A_LIST(_list))
+	{
+		return LIST_UNINITIALIZED;
+	}
+	if (NULL == _predicateFunc || NULL == _item )
+	{
+		return LIST_INV_ARG;
+	}
+	if (_list->m_head.m_next == &(_list->m_tail))
+	{
+		return LIST_IS_EMPTY;
+	}
+	curN = _list->m_tail.m_prev;
+	while(curN != &(_list->m_head))
+	{
+		isFound = _predicateFunc(curN->m_data, _context);
+		if (isFound)
+		{
+			*_item = curN->m_data;
+			return LIST_OK;
+		}
+		curN = curN->m_prev;
+	}
+	*_item = NULL;
+	return LIST_ITEM_NOT_FOUND;
+}
+
+
+
+
+
+
+
+
+
+
+/******
+LIST FOR EACH 
+******/
+static size_t CheckListForEachParams(const List* _list, UserActionFunc _action)
+{
+	if (NULL == _list || NULL == _action || MAGIC_NUM != _list->m_magicNum)
+	{
+		return 0;
+	}
+	return 1;
+}
+
+static size_t DoListForEach(const List* _list, UserActionFunc _action, void* _context)
+{
+	size_t actionCount = 0;
+	int result = 0;
+	Node* curN;
+	/*
+	TODO: check if the following if is redundant
+	*/
+	if (_list->m_head.m_next != &_list->m_tail)
+	{
+		curN = _list->m_head.m_next;
+		while(curN != &_list->m_tail)
+		{
+			result = _action(curN->m_data, _context);
+			if (result == 0)
+			{
+				return actionCount;
+			}
+			++actionCount;
+			curN = curN->m_next;
+		}
+	}
+	return actionCount;
+}
+
+size_t ListForEach(const List* _list, UserActionFunc _action, void* _context)
+{
+	if (CheckListForEachParams(_list, _action) == 0)
+	{
+		return 0;
+	}
+	return DoListForEach(_list, _action, _context);
+}
+
+
+
+
+/******
+*******
+*******
+LIST ITERATOR FUNCTIONS 
+*******
+*******
+******/
+
+/******
+LIST ITERATOR BEGIN
+******/
+ListItr ListItrBegin(const List* _list)
+{
+	if (!IS_A_LIST(_list))
+	{
+		return NULL;
+	}
+	return (ListItr) _list->m_head.m_next;
+}
+
+
+
+
+
+
+
+
+/******
+LIST ITERATOR END
+******/
+ListItr ListItrEnd(const List* _list)
+{
+	if (!IS_A_LIST(_list))
+	{
+		return NULL;
+	}
+	return (ListItr) &(_list->m_tail);
+}
+
+
+
+
+
+
+
+/******
+LIST ITERATOR END
+******/
+void* ListItrGet(ListItr _itr)
+{
+	Node* tempNode = (Node*) _itr;
+	if (NULL == _itr)
+	{
+		return NULL;
+	}
+	if (tempNode->m_next == NULL)
+	{
+		return NULL;
+	}
+	return tempNode->m_data;
+}
+
+
+
+
+
+
+
+/******
+LIST ITERATOR PREV
+******/
+ListItr ListItrPrev(ListItr _itr)
+{
+	Node* tempNode = (Node*) _itr;
+	if (NULL == _itr)
+	{
+		return NULL;
+	}
+	if (tempNode->m_prev == NULL)
+	{
+		return tempNode;
+	}
+	return tempNode->m_prev;
+}
+
+
+
+
+/*
+int ListItrEquals(const ListItr _a, const ListItr _b);
+ListItr ListItrNext(ListItr _itr);
+void* ListItrSet(ListItr _itr, void* _element);
+ListItr ListItrInsertBefore(ListItr _itr, void* _element);
+void* ListItrRemove(ListItr _itr);
+
+
+
+*/
+
+
+
+
+
+
