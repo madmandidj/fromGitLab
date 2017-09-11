08c4eeb9a967f531dfe484a8157ba5474bf15c29
Eyal Alon <madmandidj@gmail.com>
2017-09-11 12:59:24 +0300

    beforeYuval

---
+++ MT/CDR/Code/src/Processor.c
@@ -0,0 +1,16 @@
+#include "Processor.h"
+#define		PROCESSOR_MAGIC 0x0C111111
+
+struct Processor
+{
+	unsigned int 		m_magicNum;
+	unsigned int		m_numOfThreads;
+	pthread_t*			m_threadIDs;
+	Subscriber*			m_subscriber;
+	Operator*			m_operator;
+};
+
+
+
+Processor* 		ProcessorCreate(/*TODO: add parameters*/)
+
