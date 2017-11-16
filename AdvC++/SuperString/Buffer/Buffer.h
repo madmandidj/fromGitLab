db84e86d02ddecbcb763be4405f71b3c1c17df08
h47.eyalalon <madmandidj@gmail.com>
2017-11-16 08:58:40 +0200

    newclass

--- AdvC++/SuperString/Buffer/Buffer.h
+++ AdvC++/SuperString/Buffer/Buffer.h
@@ -5,6 +5,7 @@
 #include<tr1/memory>
 #include<iostream>
 #include<cstring>
+#include<math.h>
 
 /*
 Need:
@@ -30,6 +31,11 @@ public:
 	public:
 		void operator()(T* _deleteThis);
 	};
+	typedef enum ResizeMode
+	{
+		Double,
+		TenPercent	
+	}ResizeMode;
 	virtual ~Buffer();
 	explicit Buffer();
 	explicit Buffer(size_t _capacity);
@@ -39,18 +45,22 @@ public:
 	const T& GetElement(size_t _index) const;
 	inline size_t GetCapacity() const;
 	inline size_t GetSize() const;
+	inline void	SetResizeMode(ResizeMode _mode);
+	inline int GetResizeMode() const; //TODO: why does not like return ResizeMode here?
+	size_t EnsureCapacity(size_t _dataSize);
 	//TODO: AppendElement
 	//TODO: EnsureCapacity
 	//TODO: SetResizeMode //capacity grows by nearest larger multiple of 2 or by percentage
 	
 protected:
-
+	
 private:
 	Buffer(const Buffer& _buf);
 	Buffer& operator=(const Buffer& _buf);
 	std::tr1::shared_ptr<T> m_data;
 	size_t 					m_capacity;
 	size_t 					m_size;
+	ResizeMode				m_resizeMode;
 };
 
 
@@ -70,18 +80,81 @@ Buffer<T>::~Buffer()
 }
 
 template <class T> 
-Buffer<T>::Buffer() : m_capacity(1), m_size(0)
+Buffer<T>::Buffer() : m_capacity(1), m_size(0), m_resizeMode(Double)
 {	
 	std::tr1::shared_ptr<T> data (new T[m_capacity], BufferDeleter ());
 	m_data = data;
 }
 
 template <class T> 
-Buffer<T>::Buffer(size_t _capacity) :m_capacity(_capacity), m_size(0)
+Buffer<T>::Buffer(size_t _capacity) :m_capacity(_capacity), m_size(0), m_resizeMode(Double)
 {	
 	std::tr1::shared_ptr<T> data (new T[m_capacity], BufferDeleter());
 	m_data = data;
 }
+
+
+
+template <class T> 
+size_t Buffer<T>::EnsureCapacity(size_t _dataSize)
+{	
+	size_t newCapacity = 1;
+	
+	if(_dataSize > m_capacity)
+	{
+		
+		switch (m_resizeMode)
+		{
+			case Double:
+				newCapacity = 1;
+				while(newCapacity < _dataSize)
+				{
+					newCapacity *= 2;
+				}
+				break;
+				
+			case TenPercent:
+				newCapacity = _dataSize;
+				while(newCapacity < _dataSize)
+				{
+					newCapacity = (size_t)ceil(newCapacity * 1.1) ;
+				}
+				break;
+		}
+//		if (Double == m_resizeMode)
+//		{
+//			size_t newCapacity = 1;
+//			while(newCapacity < _dataSize)
+//			{
+//				newCapacity *= 2;
+//			}
+//		}
+//		else
+//		{
+//			size_t newCapacity = _dataSize;
+//			while(newCapacity < _dataSize)
+//			{
+//				newCapacity = (size_t)ceil(newCapacity * 1.1) ;
+//			}
+//		}
+		
+		std::tr1::shared_ptr<T> data (new T[newCapacity], BufferDeleter ()); //TODO: DoIfFail
+			try
+			{
+				std::copy(m_data.get(), m_data.get() + m_size, data.get());	
+			}
+			catch(std::exception& _exc)
+			{
+				throw;
+			} 
+			m_data = data;
+			m_capacity = newCapacity;
+		
+	}
+	
+	return m_capacity;
+}
+
 
 template <class T> 
 bool Buffer<T>::SetBuffer(const T* _data, size_t _dataSize)
@@ -166,6 +239,19 @@ template <class T>
 inline size_t Buffer<T>::GetSize() const
 {	
 	return m_size;
+}
+
+template <class T> 
+inline void Buffer<T>::SetResizeMode(ResizeMode _mode)
+{	
+	m_resizeMode = _mode;
+	return;
+}
+
+template <class T> 
+inline int Buffer<T>::GetResizeMode() const 
+{	
+	return m_resizeMode;
 }
 
 
