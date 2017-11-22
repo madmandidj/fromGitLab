d6cb3b63c30c3b763fd6daea6c2a37a6cb379c22
h47.eyalalon <madmandidj@gmail.com>
2017-11-22 21:52:26 +0200

    new

--- AdvC++/Super_Shared_Ptr/SuperSharedPtr.h
+++ AdvC++/Super_Shared_Ptr/SuperSharedPtr.h
@@ -4,67 +4,112 @@
 #include<stddef.h>
 namespace advcpp
 {
-
-
+//////////////////////////////////////////////////////////////////////////////////////////
+//SharedPtr forward declaration
+template<class T>
+class SharedPtr;
+//////////////////////////////////////////////////////////////////////////////////////////
+//Helper function forward declarations
+template<class T>
+void CreateOwner(SharedPtr<T>& _sp, T* _ptr);
+template<class T>
+void RemoveOwner(SharedPtr<T>& _sp);
+template<class T>
+void AddOwner(SharedPtr<T>& _thisSp, const SharedPtr<T>& _otherSp);
+//////////////////////////////////////////////////////////////////////////////////////////
+//SharedPtr class
 template<class T>
 class SharedPtr
 {
 public:
 	~SharedPtr();
-	SharedPtr();
-	SharedPtr(T* _ptr);
+	explicit SharedPtr();
+	explicit SharedPtr(T* _ptr);
 	SharedPtr(const SharedPtr& _sp);
 	SharedPtr& operator=(const SharedPtr& _sp);
 	T* const GetPtr() const;
 	T& operator*() const;
-	T* const operator->() const;
+	const SharedPtr* operator->() const;
 	bool operator==(const SharedPtr& _sp);
 	operator bool()const;
-protected:
-
 private:
 	T* 			m_ptr;
-	size_t* 	m_numOfOwners; 
-	void CreateOwner(T* _ptr);
-	void AddOwner(const SharedPtr& _sp);
-	void RemoveOwner();
+	size_t* 	m_numOfOwners;
+	friend void CreateOwner<T>(SharedPtr<T>& _sp, T* _ptr);
+	friend void RemoveOwner<T>(SharedPtr<T>& _sp);
+	friend void AddOwner<T>(SharedPtr<T>& _thisSp, const SharedPtr<T>& _otherSp); 
+//	void CreateOwner(T* _ptr);
+//	void AddOwner(const SharedPtr& _sp);
+//	void RemoveOwner();
 	void SetNullSP();
-
 };
 //////////////////////////////////////////////////////////////////////////////////////////
-//Private member functions
+//Helper function implementations //TODO: Move to seperate file
 template<class T>
-void SharedPtr<T>::CreateOwner(T* _ptr)
+inline void CreateOwner(SharedPtr<T>& _sp, T* _ptr)
 {
-	m_ptr = _ptr;
-	m_numOfOwners = new size_t;
-	*m_numOfOwners = 1;
+		_sp.m_ptr = _ptr;
+		_sp.m_numOfOwners = new size_t;
+		*_sp.m_numOfOwners = 1;
 }
 
 template<class T>
-void SharedPtr<T>::AddOwner(const SharedPtr& _sp)
+inline void RemoveOwner(SharedPtr<T>& _sp)
 {
-	m_ptr = _sp.m_ptr;
-	m_numOfOwners = _sp.m_numOfOwners;
-	__sync_add_and_fetch(m_numOfOwners, 1);
-}
-
-template<class T>
-void SharedPtr<T>::RemoveOwner()
-{
-	if(m_numOfOwners)
+	if(_sp.m_numOfOwners)
 	{
-		if (0 == __sync_sub_and_fetch(m_numOfOwners, 1))
+		if (0 == __sync_sub_and_fetch(_sp.m_numOfOwners, 1))
 		{	
-			delete m_ptr;
-			delete m_numOfOwners;
-			m_ptr = 0;	
+			delete _sp.m_ptr;
+			delete _sp.m_numOfOwners;
+			_sp.m_ptr = 0;	
 		}
 	}
 }
 
 template<class T>
-void SharedPtr<T>::SetNullSP()
+inline void AddOwner(SharedPtr<T>& _thisSp, const SharedPtr<T>& _otherSp)
+{
+	//TODO: check if pointers are valid
+	_thisSp.m_ptr = _otherSp.m_ptr;
+	_thisSp.m_numOfOwners = _otherSp.m_numOfOwners;
+	__sync_add_and_fetch(_thisSp.m_numOfOwners, 1);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//Private member functions
+//template<class T>
+//inline void SharedPtr<T>::CreateOwner(T* _ptr)
+//{
+//	m_ptr = _ptr;
+//	m_numOfOwners = new size_t;
+//	*m_numOfOwners = 1;
+//}
+
+//template<class T>
+//inline void SharedPtr<T>::AddOwner(const SharedPtr& _sp)
+//{
+//	m_ptr = _sp.m_ptr;
+//	m_numOfOwners = _sp.m_numOfOwners;
+//	__sync_add_and_fetch(m_numOfOwners, 1);
+//}
+
+//template<class T>
+//inline void SharedPtr<T>::RemoveOwner()
+//{
+//	if(m_numOfOwners)
+//	{
+//		if (0 == __sync_sub_and_fetch(m_numOfOwners, 1))
+//		{	
+//			delete m_ptr;
+//			delete m_numOfOwners;
+//			m_ptr = 0;	
+//		}
+//	}
+//}
+
+template<class T>
+inline void SharedPtr<T>::SetNullSP()
 {
 	m_ptr = 0;
 	m_numOfOwners = 0;
@@ -72,19 +117,20 @@ void SharedPtr<T>::SetNullSP()
 //////////////////////////////////////////////////////////////////////////////////////////
 //Public member functions
 template<class T>
-SharedPtr<T>::~SharedPtr()
+inline SharedPtr<T>::~SharedPtr()
 {
-	RemoveOwner();
+//	RemoveOwner();
+	RemoveOwner(*this);
 }
 
 template<class T>
-SharedPtr<T>::SharedPtr() : m_ptr(0), m_numOfOwners(0)
+inline SharedPtr<T>::SharedPtr() : m_ptr(0), m_numOfOwners(0)
 {
 	//Empty
 }
 
 template<class T>
-SharedPtr<T>::SharedPtr(T* _ptr)
+inline SharedPtr<T>::SharedPtr(T* _ptr)
 {
 	if (!_ptr)
 	{
@@ -92,12 +138,12 @@ SharedPtr<T>::SharedPtr(T* _ptr)
 	}
 	else
 	{
-		CreateOwner(_ptr);
+		CreateOwner(*this, _ptr);
 	}
 }
 
 template<class T>
-SharedPtr<T>::SharedPtr(const SharedPtr& _sp)
+inline SharedPtr<T>::SharedPtr(const SharedPtr& _sp)
 {
 	if (!_sp.GetPtr())
 	{
@@ -105,12 +151,13 @@ SharedPtr<T>::SharedPtr(const SharedPtr& _sp)
 	}
 	else
 	{	
-		AddOwner(_sp);
+//		AddOwner(_sp);
+		AddOwner(*this, _sp);
 	}
 }
 
 template<class T>
-SharedPtr<T>& SharedPtr<T>::operator=(const SharedPtr& _sp)
+inline SharedPtr<T>& SharedPtr<T>::operator=(const SharedPtr& _sp)
 {
 	if (this != &_sp)
 	{	
@@ -118,20 +165,22 @@ SharedPtr<T>& SharedPtr<T>::operator=(const SharedPtr& _sp)
 		{
 			SetNullSP();
 		}
-		RemoveOwner();
-		AddOwner(_sp);
+//		RemoveOwner();
+		RemoveOwner(*this);
+//		AddOwner(_sp);
+		AddOwner(*this, _sp);
 	}
 	return *this;
 }
 
 template<class T>
-T* const SharedPtr<T>::GetPtr() const
+inline T* const SharedPtr<T>::GetPtr() const
 {
 	return m_ptr;
 }
 
 template<class T>
-T& SharedPtr<T>::operator*() const
+inline T& SharedPtr<T>::operator*() const
 {
 	if (!GetPtr())
 	{
@@ -141,17 +190,30 @@ T& SharedPtr<T>::operator*() const
 }
 
 template<class T>
-T* const SharedPtr<T>::operator->() const
+inline const SharedPtr<T>* SharedPtr<T>::operator->() const
 {
 	if (!GetPtr())
 	{
 		//Throw exception
 	}
-	return m_ptr;
+	return this;
 }
 
+template<class T>
+inline SharedPtr<T>::operator bool() const
+{
+	if (!GetPtr())
+	{
+		//Throw exception
+	}
+	return !GetPtr() ? false : true;
+}
 
-
+template<class T>
+inline bool SharedPtr<T>::operator==(const SharedPtr& _sp)
+{
+	return m_ptr == _sp.m_ptr ? true : false;
+}
 
 
 
--- AdvC++/Super_Shared_Ptr/Test_SuperSharedPtr.cpp
+++ AdvC++/Super_Shared_Ptr/Test_SuperSharedPtr.cpp
@@ -40,8 +40,23 @@ UNIT(Operator_Arrow)
 	advcpp::SharedPtr<int> sp1(new int);
 	advcpp::SharedPtr<int> sp2;
 	sp2 = sp1;
-//	ASSERT_THAT(sp1->GetPtr() == sp2->GetPtr());
-	ASSERT_THAT(sp1->GetPtr());
+	ASSERT_THAT(sp1->GetPtr() == sp2->GetPtr());
+END_UNIT
+////////////////////////////////////////////////////////////////////////////
+UNIT(Operator_ToBool)
+	advcpp::SharedPtr<int> sp1(new int);
+	advcpp::SharedPtr<int> sp2;
+	advcpp::SharedPtr<int> sp3;
+	sp2 = sp1;
+	ASSERT_THAT((sp1) && (!sp3));
+END_UNIT
+////////////////////////////////////////////////////////////////////////////
+UNIT(Operator_Equals)
+	advcpp::SharedPtr<int> sp1(new int);
+	advcpp::SharedPtr<int> sp2;
+	advcpp::SharedPtr<int> sp3;
+	sp2 = sp1;
+	ASSERT_THAT(sp1 == sp2 && !(sp1 == sp3));
 END_UNIT
 ////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
@@ -52,6 +67,9 @@ TEST_SUITE(SuperSharedPtr)
 	TEST(Copy_CTOR)
 	TEST(Assignment_Op)
 	TEST(Operator_Dereference)
+	TEST(Operator_Arrow)
+	TEST(Operator_ToBool)
+	TEST(Operator_Equals)
 END_SUITE
 
 
