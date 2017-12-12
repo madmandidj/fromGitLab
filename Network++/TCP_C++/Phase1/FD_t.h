c5eb2af112902fb4bf83b4eff15be3ca623c3dba
eyalalon <madmandidj@gmail.com>
2017-12-12 15:15:48 +0200

    new

---
+++ Network++/TCP_C++/Phase1/FD_t.h
@@ -0,0 +1,34 @@
+#ifndef __FD_T_H__
+#define __FD_T_H__
+namespace netcpp
+{
+/////////////////////////////////////
+//////// Linux: file descriptor type
+/////////////////////////////////////
+class FD_t
+{
+public:
+	explicit FD_t();
+	FD_t(const FD_t& _fd);
+	FD_t& operator=(const FD_t& _fd);
+	FD_t& operator=(int _rawFD);
+	~FD_t();
+private:
+	int	m_nativeFd;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//namespace netcpp
+#endif//#ifndef __FD_T_H__
+
