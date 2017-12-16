cb74494c249cf12e5bbb38f70d3beda26eb4dcdb
h47.eyalalon <madmandidj@gmail.com>
2017-12-16 13:10:39 +0200

    new

--- Network++/TCP_C++/Phase2/v8_improved/Socket_t/Socket_t.h
+++ Network++/TCP_C++/Phase2/v8_improved/Socket_t/Socket_t.h
@@ -9,32 +9,39 @@ namespace netcpp
 class Socket_t
 {
 public:
-/*	enum SOCKTERR*/
-/*	{*/
-/*		SOCKTERR_EAGAIN*/
-/*		, SOCKTERR_SELECTFAIL*/
-/*		, SOCKTERR_SOCKETFAIL*/
-/*		, SOCKTERR_CONNECTFAIL*/
-/*		, SOCKTERR_SETSOCKOPTFAIL*/
-/*		, SOCKTERR_BINDFAIL*/
-/*		, SOCKTERR_LISTENFAIL*/
-/*		, SOCKTERR_ISCONNECTED*/
-/*		, SOCKTERR_ISDISCONNECTED*/
-/*		, SOCKTERR_BROKENPIPE*/
-/*		, SOCKTERR_TOOMANYOPENFILES*/
-/*		, SOCKTERR_UNKNOWNERRNO*/
-/*	}SOCKTERR;*/
 	explicit Socket_t();
-	Socket_t(const Socket_t& _socket);
-	Socket_t& operator=(const Socket_t& _socket);
 	virtual ~Socket_t();
 	friend class FDSet_t;
 protected:
 	FD_t 			m_fd;
 	SockAddrIn_t	m_sin;
 	bool			m_isConnected;
+private:
+	Socket_t(const Socket_t& _socket);
+	Socket_t& operator=(const Socket_t& _socket);
 };
 }//namespace netcpp
 #endif//#ifndef __SOCKET_T_H__
+
+//TODO: Remove this if not needed
+//	enum SOCKTERR
+//	{
+//		SOCKTERR_EAGAIN
+//		, SOCKTERR_SELECTFAIL
+//		, SOCKTERR_SOCKETFAIL
+//		, SOCKTERR_CONNECTFAIL
+//		, SOCKTERR_SETSOCKOPTFAIL
+//		, SOCKTERR_BINDFAIL
+//		, SOCKTERR_LISTENFAIL
+//		, SOCKTERR_ISCONNECTED
+//		, SOCKTERR_ISDISCONNECTED
+//		, SOCKTERR_BROKENPIPE
+//		, SOCKTERR_TOOMANYOPENFILES
+//		, SOCKTERR_UNKNOWNERRNO
+//	}SOCKTERR;
+
+
+
+
 
 
