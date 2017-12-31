faeb6a9eae543f2f82bf240b82fd0fbf712a2ddb
eyalalon <madmandidj@gmail.com>
2017-12-31 17:05:42 +0200

    new

---
+++ PerformanceToolsAndAdvGDB/Exercises/DivByZero/DivByZero.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+	int numToDivide = 100;
+	int result;
+	result = numToDivide / 0;
+	return 0;
+}
---
+++ PerformanceToolsAndAdvGDB/Exercises/InfiniteLoop/InfiniteLoop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+	int i = 0;
+	int j = 1;
+	while(1)
+	{
+		++j;
+	}
+	return 0;
+}
