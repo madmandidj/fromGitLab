f103a61defde8f61e75502c5268d606108a8d39b
madmandidj <madmandidj@gmail.com>
2018-03-28 21:50:55 +0300

    start of heap

---
+++ Eyals_Coding/Data_Structures/inc/Heap.h
@@ -0,0 +1,26 @@
+#ifndef __HEAP_H__
+#define __HEAP_H__
+
+#include "ADTErr.h"
+#include "Vector.h"
+
+typedef struct Heap Heap;
+typedef int (*ShouldSwapFunc)(void*, void*);
+
+Heap* HeapBuild(Vector* _vec, ShouldSwapFunc _shouldSwapFunc);
+
+void HeapDestroy(Heap* _heap);
+
+ADTErr HeapInsert(Heap* _heap, void* _element);
+
+ADTErr HeapMax(Heap* _heap, void** _maxElement);
+
+ADTErr HeapExtractMax(Heap* _heap, void** _maxElement);
+
+size_t HeapItemsNum(Heap* _heap);
+
+void HeapPrint(Heap* _heap);
+
+
+#endif /*#ifndef __HEAP_H__*/
+
---
+++ Eyals_Coding/Data_Structures/source/Heap/TEST_Heap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "../../inc/Vector.h"
+#include "../../inc/Heap.h"
+#include "../../inc/mu_test.h"
+#include "../../inc/ADTErr.h"
+
+
+
+UNIT(Heap_Create)
+    ASSERT_THAT(1);
+END_UNIT
+
+
+
+TEST_SUITE(List tests)
+	TEST(Heap_Create)
+END_SUITE
