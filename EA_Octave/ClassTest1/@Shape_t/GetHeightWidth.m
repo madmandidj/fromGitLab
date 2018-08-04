13f0bd26be09910eca9f9de98a647b2695c1bab8
madmandidj <madmandidj@gmail.com>
2018-08-04 23:16:28 +0300

    new

--- EA_Octave/ClassTest1/@Shape_t/GetHeightWidth.m
+++
@@ -1,6 +0,0 @@
-methods (Access = private)
-        function [outHeight,outWidth] = GetHeightWidth(_this)
-            outHeight = _this.height;
-            outWidth = _this.width;
-        endfunction
-endmethods
--- EA_Octave/ClassTest1/@Shape_t/Shape_t.m
+++ EA_Octave/ClassTest1/@Shape_t/Shape_t.m
@@ -1,33 +1,36 @@
 classdef Shape_t
-    properties (Access = private)
-        height = 8;
-        width = -8;
-        str1 = "string1";
-        strCells = cell(10,1);
-    endproperties
-
     methods
         function instance = Shape_t(_shapeName)
+          if (!exist("_shapeName","var"))
+            instance.height = 171;
+            instance.width = 171;
+          else
             switch (_shapeName)
+                
                 case "Rectangle_t"
                     instance.height = 8;
                     instance.width = 5;
-                % otherwise
-                %     instance.height = 0;
-                %     instance.width = 0;
             endswitch
+          endif
         endfunction
         function disp(_this)
             printf("Height = %d\n", (_this.height));
             printf("Width = %d\n",_this.width);
         endfunction
     endmethods
-
-    % methods (Access = private)
-    %     function [outHeight,outWidth] = GetHeightWidth(_this)
-    %         outHeight = _this.height;
-    %         outWidth = _this.width;
-    %     endfunction
-    % endmethods
+    
+    methods (Access = private)
+        function [outHeight,outWidth] = GetHeightWidth(_this)
+            outHeight = _this.height;
+            outWidth = _this.width;
+        endfunction
+    endmethods
+    
+    properties (Access = private)
+        height = 8;
+        width = -8;
+        str1 = "string1";
+        strCells = cell(10,1);
+    endproperties
 
 endclassdef
